7f1bf28ac3d00f8a4e70e870b054dcd244d48374
Rifat Hasan <rifat562hasan@gmail.com>
2020-12-02 20:30:03 -0800

    First Bot Test

---
+++ bot.js
@@ -0,0 +1,33 @@
+require("dotenv").config()
+
+const Discord = require("discord.js")
+const client = new Discord.Client({
+  partials: ["MESSAGE"]
+})
+
+const BOT_PREFIX = ">"
+const MOD_ME_COMMAND = "mod-me"
+
+client.on("ready", () => {
+  console.log("Our bot is ready to go!!!!")
+})
+
+client.on("messageDelete", msg => {
+  msg.channel.send("Stop deleting messages!")
+})
+
+client.on("message", msg => {
+  if (msg.content == "moron") {
+    msg.react("😈")
+  }
+
+  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+    modUser(msg.member)
+  }
+})
+
+function modUser(member) {
+  member.roles.add("783901492175831080")
+}
+
+client.login(process.env.BOT_TOKEN)
